39d5d9095e763e8c2a1d67fd5574500e6a043b2c
amyard <maksymturenko@gmail.com>
2020-07-12 12:25:05 +0300

    add custom middleware to handle with errors

---
+++ skinet/API/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFoundRequuest()
+        {
+            var thing = _context.Products.Find(42);
+            if(thing == null)
+            {
+                return  NotFound(new ApiResponse(404));
+            }
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public IActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+            var thingToReturn = thing.ToString();       // will generate Exception
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+        
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetNotFoundRequuest(int id)
+        {
+            return Ok();
+        }
+    }
+}
