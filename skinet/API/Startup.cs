5e80acb79c68f869a62660dc1768b62ee48e6839
unknown <maksymturenko@gmail.com>
2020-08-04 23:47:23 +0300

    add backet. using Redis for working with backet.

--- skinet/API/Startup.cs
+++ skinet/API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -28,6 +29,12 @@ namespace API
             services.AddApplicationServices();       // get from our custom extension folder
 
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            // using Redis for basket
+            services.AddSingleton<IConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
 
             services.AddSwaggerDocumentation();      // get from our custom extension folder
             services.AddCors(opt=>{
