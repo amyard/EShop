622f8c11b71975ca28c88aeeb0ab7a031718685a
amyard <maksymturenko@gmail.com>
2020-08-27 01:57:05 +0300

    order - add additional DTO

--- skinet/API/Helpers/MappingProfiles.cs
+++ skinet/API/Helpers/MappingProfiles.cs
@@ -1,7 +1,7 @@
 using API.Dtos;
 using AutoMapper;
 using Core.Enteties;
-using Core.Enteties.Identity;
+using Core.Enteties.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -14,10 +14,17 @@ namespace API.Helpers
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Enteties.Identity.Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
             CreateMap<AddressDto, Core.Enteties.OrderAggregate.Address>();
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl));
         }
     }
 }
