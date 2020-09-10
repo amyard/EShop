d5b694c1233b6aa08b1a55d6cc4b2895e4d03348
amyard <maksymturenko@gmail.com>
2020-09-11 02:03:30 +0300

    ang - click on type/brand

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
+import { map } from 'rxjs/operators';
 import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models//productType';
 
@@ -12,8 +13,23 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
   getBrands() {
